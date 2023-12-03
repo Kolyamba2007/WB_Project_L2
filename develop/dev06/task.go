19954af6b86c21b85be39628a7a9d4bb13f98540
Ilya <55401238+Kolyamba2007@users.noreply.github.com>
2023-12-03 17:13:57 +0300

    Update task.go

--- develop/dev06/task.go
+++ develop/dev06/task.go
@@ -1,5 +1,48 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
+func main() {
+	// He was extraordinarily particular\nabout6politeness6in6others
+
+	reader := bufio.NewReader(os.Stdin)
+	str, err := reader.ReadString('/')
+	if err != nil {
+		panic(err)
+	}
+
+	str, _ = strings.CutSuffix(str, "/")
+	fmt.Println(str)
+
+	//result := Mancut(&line, []int{2, 4}, " ", false) //ошибка колонка меньше 1
+	//fmt.Println(result)
+}
+
+func Mancut(line *string, fields []int, delimiter string, separated bool) (result []string) {
+	strings1 := strings.Split(*line, "\n")
+	fmt.Println(strings1)
+
+	for _, str := range strings1 {
+		if strings.Contains(str, delimiter) {
+			tempStringSlice := strings.Split(str, delimiter)
+
+			var tempString string
+			for _, field := range fields {
+				if len(tempStringSlice) >= field {
+					tempString += tempStringSlice[field-1]
+				}
+			}
+
+			result = append(result, tempString)
+		} else if !separated {
+			result = append(result, str)
+		}
+	}
+
+	return
 }
